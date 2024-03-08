d10bce7fde1fdfef3079055066ba6035b2e421c3
gladio0501 <v.miekh0501@gail.com>
2024-03-08 16:49:59 +0100

    Modification 3

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more inform
 
 Console.WriteLine("Hello, World!");
+for (int i = 0; i < 5; i++) 
+{
+    Console.WriteLine(i);
+}
+// My mum is awesome 
