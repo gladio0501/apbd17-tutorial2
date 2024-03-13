c001f7ee05b4f4705b1bbab0865fc3b615c02a2e
Vladyslav Miekh <107705243+gladio0501@users.noreply.github.com>
2024-03-13 18:29:00 +0100

    Added FindMax function

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more inform
+﻿
 
 Console.WriteLine("Hello, World!");
 for (int i = 0; i < 5; i++) 
@@ -16,3 +16,25 @@ static double GetAverage(int[] numbers)
 
     return sum / numbers.Length;
 }
+
+static int FindMax(int[] array)
+{
+    if (array == null || array.Length == 0)
+    {
+        throw new ArgumentException("Array cannot be null or empty");
+    }
+
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int[] numbers = { 5, 3, 9, 2, 7 };
+int max = FindMax(numbers);
+Console.WriteLine("The maximum value is: " + max);
