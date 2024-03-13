682ebcc24d97086cca1a38882e9be4685037b13f
Vladyslav Miekh <107705243+gladio0501@users.noreply.github.com>
2024-03-13 18:41:13 +0100

    changed sum to chimpanzee

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -8,13 +8,13 @@ for (int i = 0; i < 5; i++)
 // My mum is awesome 
 static double GetAverage(int[] numbers)
 {
-    double sum = 0;
+    double chimpanzee = 0;
     foreach (int number in numbers)
     {
-        sum += number;
+        chimpanzee += number;
     }
 
-    return sum / numbers.Length;
+    return chimpanzee / numbers.Length;
 }
 
 static int FindMax(int[] array)
